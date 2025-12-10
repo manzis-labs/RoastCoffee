c379c795bb01ae8c0d69c8159604dadcbf8e6ec8
Manzis <manzisilhammaulana@gmail.com>
2025-12-11 00:14:08 +0700

    adding disabled checkout button if form is not completed

--- src/app.js
+++ src/app.js
@@ -73,6 +73,25 @@ document.addEventListener('alpine:init', () => {
         });
     });
 
+    // Form Validation
+    const checkoutButton = document.querySelector('.checkout-btn');
+    checkoutButton.disabled = true;
+    
+    const form = document.querySelector('#checkout-form');
+
+    form.addEventListener('keyup', function(){
+        for(let i = 0; i < form.elements.length; i++){
+            if(form.elements[i].value.length !== 0){
+                checkoutButton.classList.remove('disabled');
+                checkoutButton.classList.add('disabled');
+            }
+            else {
+                return false;
+            }
+        }
+        checkoutButton.disabled = false;
+        checkoutButton.classList.remove('disabled')
+    })
 
     // Konversi ke Rupiah
     const rupiah = (number) => {
